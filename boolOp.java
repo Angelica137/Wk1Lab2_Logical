bb6aa84688f24d51dcf8593b7d1f6d4bc2b4d67f
Angelica <Angelica137@users.noreply.github.com>
2021-03-14 13:36:10 +0000

    declare variables

--- boolOp.java
+++ boolOp.java
@@ -1,23 +1,24 @@
 
 /**
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class boolOp
-{
-    //Put your data type declarations here - I have put bresult there to hold the results 
-    // of your calculations (see the Boolbag example)
-      boolean bresult;
+public class boolOp {
+	// Put your data type declarations here - I have put bresult there to hold the
+	// results
+	// of your calculations (see the Boolbag example)
+	boolean bresult;
+	boolean finished = false;
+	int m = 3;
+	int n = 8;
 
+	/*
+	 * Put the code for your calculations in this method. The result of the
+	 * expression should be calculated and assigned to 'bresult'
+	 */
+	void a_boolOperation() {
 
-/* Put the code for your calculations in this method.
- * The result of the expression should be calculated and assigned to 'bresult'
- */
-   void a_boolOperation()
-   {
-        
-   }
-    
-	
+	}
+
 }
